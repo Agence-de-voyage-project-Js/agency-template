e40f822d5e4492d1ca20b7b4a20a195d6270893b
wafaslim <js.wafa@gmail.com>
2020-12-21 14:58:52 +0100

    val register

--- js/register.js
+++ js/register.js
@@ -5,16 +5,20 @@ const pass2=document.querySelector('#pass2')
 const form=document.querySelector('form')
 
 
+
+
 form.addEventListener('submit', e => {
  e.preventDefault();
  if(pass.value==pass2.value){
     const iemail = email.value.trim();
     const ipassword = pass.value.trim();
+    console.log(iemail);
         const user={
            email : iemail,
            password : ipassword,
            id: Math.floor(Math.random()*1000),
-           role:'user'
+           role:'user',
+           connected:'true',
         };
     const existant = users.find(user => user.email===iemail)
     if (!existant){
@@ -25,6 +29,7 @@ form.addEventListener('submit', e => {
      else{
          alert('Email deja existant')
      }
+     
   }
   else{
     alert('Logging ou mot de passe incorrect')
