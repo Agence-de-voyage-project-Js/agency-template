24696ec36c57abd5cdaf16aece974090ebd0e54c
NasserImen <73170627+NasserImen@users.noreply.github.com>
2020-12-21 15:31:44 +0100

    Verification Login

---
+++ login.js
@@ -0,0 +1,23 @@
+var users = JSON.parse(localStorage.getItem('users')) || [];
+
+const formLogin = document.querySelector('form')
+
+
+
+
+
+formLogin.addEventListener('submit' , (e) =>{
+    e.preventDefault();
+var userInfo = users.find(user => user.email === formLogin.email1.value && user.password === formLogin.password1.value)
+console.log(userInfo);
+if (userInfo){
+    var userConnecte ={ email:userInfo.email , id:userInfo.id}
+    localStorage.setItem('Connect', JSON.stringify(userConnecte))
+    console.log(userConnecte);
+    location.href='index.html';
+}
+else{
+    alert('Email or Password not found')
+}
+})
+
