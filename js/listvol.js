bc08f298a9cecc2111824cbcd2a5fc3c8a655214
wafaslim <js.wafa@gmail.com>
2020-12-23 12:21:38 +0100

    refactor list vol

--- js/listvol.js
+++ js/listvol.js
@@ -1,22 +1,26 @@
 function affichVol(){
   const vols = JSON.parse(localStorage.getItem('vols')) || []
   const infovol = document.getElementById('vol')
-  data='';
   if (vols.length > 0) {
-      for (i = 0; i < vols.length; i++) {
-        data += '<tr>';
-        data += '<td>' + vols[i].numVol + '</td>';
+    vols.forEach((vol, index) => {
+      const ligne = document.createElement('tr');
+        data='';
+        
+        data += '<td>' + vol.numVol + '</td>';
         data += '<td>' + 'DEPART' + '</td>'
-        data += '<td>' + vols[i].hVoldepart + '</td>';
-        data += '<td>' + vols[i].departSelection + '</td>';
+        data += '<td>' + vol.hVoldepart + '</td>';
+        data += '<td>' + vol.departSelection + '</td>';
         data += '<td>' + 'ARRIVEE' + '</td>'
-        data += '<td>' + vols[i].hVolarrivee + '</td>';
-        data += '<td>' + vols[i].arriveeSelection + '</td>'
+        data += '<td>' + vol.hVolarrivee + '</td>';
+        data += '<td>' + vol.arriveeSelection + '</td>'
         data += '<td><i class="fas fa-trash delete">Edit</i></td>';
         data += '<td><i class="fas fa-pen edit">Delete</i></td>';
-        data += '</tr>';
-      }
+        
+        ligne.innerHTML = data;
+
+        infovol.appendChild(ligne)
+      });
     }
-    return infovol.innerHTML = data;
 }
-console.log(affichVol());
+
+affichVol();
