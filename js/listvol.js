14037ea1a571b02d92e1f1bdead880ceb2306654
sofienkarka <sofien.gallas@gmail.com>
2021-01-05 14:53:19 +0100

    edite updatevol

--- js/listvol.js
+++ js/listvol.js
@@ -1,22 +1,23 @@
-function affichVol(){
-  const vols = JSON.parse(localStorage.getItem('vols')) || []
-  const infovol = document.getElementById('vol')
-  data='';
+function affichVol() {
+  const vols = JSON.parse(localStorage.getItem("vols")) || [];
+  const infovol = document.getElementById("vol");
+  data = "";
   if (vols.length > 0) {
-      for (i = 0; i < vols.length; i++) {
-        data += '<tr>';
-        data += '<td>' + vols[i].numVol + '</td>';
-        data += '<td>' + 'DEPART' + '</td>'
-        data += '<td>' + vols[i].hVoldepart + '</td>';
-        data += '<td>' + vols[i].departSelection + '</td>';
-        data += '<td>' + 'ARRIVEE' + '</td>'
-        data += '<td>' + vols[i].hVolarrivee + '</td>';
-        data += '<td>' + vols[i].arriveeSelection + '</td>'
-        data += '<td><i class="fas fa-trash delete">Edit</i></td>';
-        data += '<td><i class="fas fa-pen edit">Delete</i></td>';
-        data += '</tr>';
-      }
+    for (i = 0; i < vols.length; i++) {
+      data += "<tr>";
+      data += "<td>" + vols[i].numVol + "</td>";
+      data += "<td>" + "DEPART" + "</td>";
+      data += "<td>" + vols[i].hVoldepart + "</td>";
+      data += "<td>" + vols[i].departSelection + "</td>";
+      data += "<td>" + "ARRIVEE" + "</td>";
+      data += "<td>" + vols[i].hVolarrivee + "</td>";
+      data += "<td>" + vols[i].arriveeSelection + "</td>";
+      data +=
+        '<td><button data-toggle ="modal" data-target="#exampleModal2"><i class="fas fa-trash edit">Edit</i></button></td>';
+      data += '<td><i class="fas fa-pen delete ">Delete</i></td>';
+      data += "</tr>";
     }
-    return infovol.innerHTML = data;
+  }
+  return (infovol.innerHTML = data);
 }
 console.log(affichVol());
