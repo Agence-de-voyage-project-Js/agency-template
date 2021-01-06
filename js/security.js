2ad7e191f56809f280899645e4f20c099f63fa94
wafaslim <js.wafa@gmail.com>
2021-01-06 12:46:56 +0100

    modification book flight

--- js/security.js
+++ js/security.js
@@ -1,17 +1,15 @@
 function connected(){
-    const connectedUser = localStorage.getItem('connectedUser')
+    const connectedUser = JSON.parse(localStorage.getItem('connectedUser'));
     if ( !connectedUser ){
         location.href='login.html';
     }
 }
 function connectedAdmin(){
-    const Users = localStorage.getItem('users')
-    console.log(Users);
-    if (user!==null){
-        
-        if (users.role !== 'admin'){
-            console.log(users.role);
-            location.href = 'login.html';
+    const connectedUser = JSON.parse(localStorage.getItem('connectedUser'));
+    if (connectedUser!==null){
+        // console.log(connectedUser);
+        if (connectedUser.role != 'admin'){
+            location.href = 'index.html';
         }
         
     }
