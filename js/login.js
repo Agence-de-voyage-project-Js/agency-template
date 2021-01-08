1b9d6436dac48c646648e1ab2b33f4fc941baf06
sofienkarka <sofien.gallas@gmail.com>
2021-01-08 16:38:45 +0100

    edit login user

--- js/login.js
+++ js/login.js
@@ -1,20 +1,19 @@
-var users = JSON.parse(localStorage.getItem('users')) || [];
-const formLogin = document.querySelector('form')
+var users = JSON.parse(localStorage.getItem("users")) || [];
+const formLogin = document.querySelector("form");
 
-formLogin.addEventListener('submit' , (e) =>{
-    e.preventDefault();
-var userFound = users.find(user => user.email === formLogin.email1.value && user.password === formLogin.password1.value)
-if (userFound !== undefined){
-    localStorage.setItem('connectedUser', JSON.stringify(userFound))
-    if (userFound.role == 'admin'){
-        location.href = 'pageAdmin.html';
+formLogin.addEventListener("submit", (e) => {
+  e.preventDefault();
+  var userFound = users.find(
+    (user) => user.email === formLogin.email1.value && user.password === formLogin.password1.value
+  );
+  if (userFound !== undefined) {
+    localStorage.setItem("connectedUser", JSON.stringify(userFound));
+    if (userFound.role == "admin") {
+      location.href = "pageAdmin.html";
+    } else if ((history.go(-1) = !"index.html")) {
+      location.href = "listFlight.html";
     }
-    else{
-    location.href='index.html';
-    }
-}
-else{
-    alert('Email or Password not found')
-}
-})
-
+  } else {
+    alert("Email or Password not found");
+  }
+});
